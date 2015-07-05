bacd070d51a98a160480ad3ac549ab4b7ddbac56
SmellyCat <smellycat2014@foxmail.com>
2015-07-05 21:44:11 +0800

    Add hpzl,csys and cllx api

--- application/models/mcgs.php
+++ application/models/mcgs.php
@@ -22,7 +22,19 @@ class Mcgs extends CI_Model
      */
 	public function getVehicle($data)
 	{	
-		$this->cgs_db->select('*');
+		$this->cgs_db->select('id');
+		$this->cgs_db->select('hpzl');
+		$this->cgs_db->select('hphm');
+		$this->cgs_db->select('clpp1');
+		$this->cgs_db->select('clpp2');
+		$this->cgs_db->select('clxh');
+		$this->cgs_db->select('zzcmc');
+		$this->cgs_db->select('clsbdh');
+		$this->cgs_db->select('fdjh');
+		$this->cgs_db->select('cllx');
+		$this->cgs_db->select('csys');
+		$this->cgs_db->select('syr');
+		$this->cgs_db->select('fzrq');
 		$this->cgs_db->from('vehicle_gd');
 		$this->cgs_db->where('hphm', $data['hphm']);
 		switch (@$data['hpys']) {
@@ -52,6 +64,40 @@ class Mcgs extends CI_Model
 		return $this->cgs_db->get();
 	}
 
+    /**
+     * 获取号牌种类
+     * 
+     * @return object
+     */
+	public function getHpzl()
+	{	
+		$this->cgs_db->select('hpzl.*');
+		$this->cgs_db->select('hpys.color');
+		$this->cgs_db->join('hpys', 'hpys.id = hpzl.color_id');
+		return $this->cgs_db->get('hpzl');
+	}
+
+    /**
+     * 获取车身颜色
+     * 
+     * @return object
+     */
+	public function getCsys()
+	{	
+		$this->cgs_db->select('*');
+		return $this->cgs_db->get('csys');
+	}
+
+    /**
+     * 获取车辆类型
+     * 
+     * @return object
+     */
+	public function getCllx()
+	{	
+		$this->cgs_db->select('*');
+		return $this->cgs_db->get('d_cllx');
+	}
 }
 ?>
 
