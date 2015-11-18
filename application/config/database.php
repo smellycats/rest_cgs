ee650bccfa508b609099d90d6e10f6e5e93ddefd
SmellyCat <smellycat2014@foxmail.com>
2015-11-18 23:19:17 +0800

    Add hbc_db

--- application/config/database.php
+++ application/config/database.php
@@ -105,4 +105,26 @@ $db['cgs_db'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['hbc_db'] = array(
+	'dsn'	=> '',
+	'hostname' => '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SID=kakou)))',
+	'username' => 'kakou',
+	'password' => 'kakou',
+	'database' => 'kakou',
+	'dbdriver' => 'oci8',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
  
