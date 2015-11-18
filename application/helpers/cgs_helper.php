88615f8310dabc4ecbd86a6ec295b01ea3b304bf
SmellyCat <smellycat2014@foxmail.com>
2015-11-18 23:27:01 +0800

    Add methods h_convert_param and h_convert_url_query

--- application/helpers/cgs_helper.php
+++ application/helpers/cgs_helper.php
@@ -69,3 +69,61 @@ if ( ! function_exists('h_convertParam'))
         return $params;
     }
 }
+
+/**
+ * h_convert_param
+ *
+ * q 参数转换成数组
+ *
+ * @access  public
+ * @param   string
+ * @return  array
+ */
+
+if ( ! function_exists('h_convert_param'))
+{
+    function h_convert_param($q)
+    {
+        $queryParts = explode('+', $q);
+
+        $params = array();
+        foreach ($queryParts as $id=>$param) {
+            if ($id == 0) {
+                $params['q'] = $param;
+            } else {
+                $key = strstr($param, ':', true);
+                if ($key) {
+                    $val = substr(strstr($param, ':'), 1);
+                    $params[$key] = $val;
+                }
+            }
+        }
+        return $params;
+    }
+}
+
+/**
+ * h_convert_url_query
+ *
+ * url 参数转换成数组
+ *
+ * @access  public
+ * @param   string
+ * @return  array
+ */
+
+if ( ! function_exists('h_convert_url_query'))
+{
+    function h_convert_url_query($query)
+    {
+        $queryParts = explode('&', $query);
+
+        $params = array();
+
+        foreach ($queryParts as $param) {
+            $item = explode('=', $param);
+            $params[$item[0]] = @$item[1];
+        }
+        return $params;
+    }
+}
